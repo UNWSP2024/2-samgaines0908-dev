91a3d2a06470a11e0c7b9905c1dd4a368b3e3c21
samgaines0908-dev <sam.gaines0908@gmail.com>
2026-01-30 12:00:57 -0600

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -2,13 +2,13 @@
     #Date: 1/30/2026
     #Title: Average age
 def average_age():
-    # Get User Input
+    #Get User Input
     age1 = int(input("Enter your age friend 1: "))
     age2 = int(input("Enter your age friend 2: "))
     age3 = int(input("Enter your age friend 3: "))
     age4 = int(input("Enter your age friend 4: "))
     age5 = int(input("Enter your age friend 5: "))
-    # Sum ages
+    #Sum ages
     sum_of_age= age1+age2+age3+age4+age5
     # Average the ages
     average_age = sum_of_age/5.0
